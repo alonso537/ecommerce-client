41d279c7f83ee3c39887719f1b6148a6a568ff39
alonso537 <adrianaalonso002mail.com>
2023-08-31 07:23:09 -0600

    inicio and sass

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+// import '@/styles/globals.css'
+import '@/scss/global.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
