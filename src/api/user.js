51d9083073a3342dc0c4a28e68f027463deb5438
alonso537 <adrianaalonso002mail.com>
2023-09-02 17:03:01 -0600

    authFetcher

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,20 @@
+import { authFetch } from "@/utils/authFetcher";
 const { ENV } = require("@/utils/constanst");
+
+
+
 
 export class User {
   async getMe(token) {
     try {
       const url = `${ENV.API_URL}${ENV.ENDPOINTS.USERS_ME}`;
-      const params = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
+      // const params = {
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //   },
+      // };
 
-      const response = await fetch(url, params);
+      const response = await authFetch(url);
       const result = await response.json();
 
       if (response.status !== 200) throw result;
