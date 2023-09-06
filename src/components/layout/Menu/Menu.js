3ae3a2d4b7c9b8a4c23f2cabfba07cbbae7adba6
alonso537 <adrianaalonso002mail.com>
2023-09-06 07:35:18 -0600

    buscador

--- src/components/layout/Menu/Menu.js
+++ src/components/layout/Menu/Menu.js
@@ -1,46 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import styles from './Menu.module.scss'
-import { Platform } from '@/api/platform'
-import Link from 'next/link'
-import { Image } from 'semantic-ui-react'
-import { map } from 'lodash'
+import React, { useEffect, useState } from "react";
+import styles from "./Menu.module.scss";
+import { Platform } from "@/api/platform";
+import Link from "next/link";
+import { Button, Icon, Image, Input } from "semantic-ui-react";
+import { map } from "lodash";
+import classNames from "classnames";
 
+const platformCtrl = new Platform();
 
-const platformCtrl = new Platform()
+const Menu = ({ isOpenSearch }) => {
+  const [platforms, setPlatforms] = useState(null);
+  const [showSearch, setShowSearch] = useState(false)
 
-const Menu = ({isOpenSearch}) => {
-    const [platforms, setPlatforms] = useState(null)
+  const openCloseSearch = () => setShowSearch((prevState) => !prevState)
 
-    useEffect(() => {
-      (async () => {
-        try {
-            //TODO: peticion...
-            const response = await platformCtrl.getAll()
-            setPlatforms(response)
-        } catch (error) {
-            console.log(error);
-        }
-      })()
-    }, [])
-    
-    console.log(platforms);
+  useEffect(() => {
+    (async () => {
+      try {
+        //TODO: peticion...
+        const response = await platformCtrl.getAll();
+        setPlatforms(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  // console.log(platforms);
 
   return (
     <div className={styles.platforms}>
-        {
-            map(platforms?.data, (platform) => {
-                return (
-                    <Link key={platform.id} href={`/games/${platform.attributes.slug}`}>
-                    <Image src={platform?.attributes.icon.data.attributes.url} />
-                    {
-                        platform?.attributes?.title
-                    }
-                </Link>
-                )
-            })
-        }
-    </div>
-  )
-}
+      {map(platforms?.data, (platform) => {
+        return (
+          <Link key={platform.id} href={`/games/${platform.attributes.slug}`}>
+            <Image src={platform?.attributes.icon.data.attributes.url} />
+            {platform?.attributes?.title}
+          </Link>
+        );
+      })}
 
-export default Menu
+      <button className={styles.search} onClick={openCloseSearch}>
+        <Icon name="search" />
+      </button>
+
+      <div className={classNames(styles.inputContainer, {
+        [styles.active] : showSearch
+      })}>
+        <Input
+          id="search-games"
+          placeholder="Buscador"
+          className={styles.input}
+          focus={true}
+        />
+        <Icon
+          name="close"
+          className={styles.closeInput}
+          onClick={openCloseSearch}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
