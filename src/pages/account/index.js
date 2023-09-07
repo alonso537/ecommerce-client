6b225a3bc2d6110bce141627b959556723f9d941
alonso537 <adrianaalonso002mail.com>
2023-09-07 07:48:53 -0600

    tabs

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,11 +1,79 @@
 import Info from '@/components/Account/Info/Info'
 import BasicLayout from '@/layouts/BasicLayout/BasicLayout'
 import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import styles from './Account.module.scss';
+import { useAuth } from '@/hooks/userAuth';
+import { useRouter } from 'next/router';
 
 const Account = () => {
+
+  const {user, logout} = useAuth()
+  const router = useRouter()
+
+
+  if(!user) {
+    router.push('/')
+    return null
+  }
+
+  const onLogout = () => {
+    logout()
+    router.push('/join/sign-in')
+  }
+
+  const panes = [
+    {
+      menuItem: 'Mis Pedidos',
+      render: () => (
+        <Tab.Pane attached={false}>
+          <p>Mis Pedidos...</p>
+
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: 'Lista de deseos',
+      render: () => (
+        <Tab.Pane attached={false}>
+          <p>Lista de deseos...</p>
+
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: 'Direcciones',
+      render: () => (
+        <Tab.Pane attached={false}>
+          <p>direcciones...</p>
+
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: {icon: 'settings', content: 'Ajustes'},
+      render: () => (
+        <Tab.Pane attached={false}>
+          <p>Ajustes</p>
+
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: {
+        icon: 'logout',
+        content: '',
+        onClick: onLogout
+      }
+    }
+  ]
+
+
   return (
     <BasicLayout isContainer relative>
       <Info />
+
+      <Tab menu={{secondary: true, pointing: true} } panes={panes} className={styles.tabs} />
     </BasicLayout>
   )
 }
