acafb6f3f5b898c446330d6bff7f9bd72701a22b
alonso537 <adrianaalonso002mail.com>
2023-09-07 07:35:54 -0600

    user info

---
+++ src/pages/account/index.js
@@ -0,0 +1,13 @@
+import Info from '@/components/Account/Info/Info'
+import BasicLayout from '@/layouts/BasicLayout/BasicLayout'
+import React from 'react'
+
+const Account = () => {
+  return (
+    <BasicLayout isContainer relative>
+      <Info />
+    </BasicLayout>
+  )
+}
+
+export default Account
